cebf8111e2dcb38248617be87b4ac9b37fb0ca69
unknown <majid_sp2007@yahoo.com>
2020-01-29 10:42:58 +0100

    Working on Spinner component to show when loading time.

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -12,7 +12,6 @@ export const selectCollectionsForPreview = createSelector(
     collections => collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
-
 export const selectCollection = collectionUrlParam => createSelector (
     [selectCollections],
     collections => collections ? collections[collectionUrlParam] : null

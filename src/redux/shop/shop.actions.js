9ed069d59d546e26572d0b08650d319b162ea80a
unknown <majid_sp2007@yahoo.com>
2020-02-02 16:09:21 +0100

    Some clean up work

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -16,6 +16,7 @@ export const fetchCollectionsFailure = errorMessage => ({
   payload: errorMessage
 });
 
+
 export const fetchCollectionsStartAsync = () => {
   return dispatch => {
     const collectionRef = firestore.collection('collections');
